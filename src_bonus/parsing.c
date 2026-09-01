ad0375619ddc483067984d8d2257eeebb70629c9
gcassi-d <gcassi-d@student.42urduliz.com>
2026-09-01 19:15:49 +0200

    now I think yes

--- src_bonus/parsing.c
+++ src_bonus/parsing.c
@@ -99,12 +99,6 @@ char* parse(char* input) {
 	while (isspace(input[i]))
 		i++;
 	while (input[i]) {
-		if (last_op && (isop(input[i]) || isespop(input[i]))) {
-				bad_char("Unexpected operator", input[i]);
-				free(buf.str);
-				return NULL;
-			}
-
 		if (ft_isdigit(input[i])) {
 			if (append_num(input, &i, &buf))
 				return NULL;
@@ -214,6 +208,11 @@ char* parse(char* input) {
 					return NULL;
 				}
 
+				if (isop(input[i]) || isespop(input[i])) {
+					bad_char("Unexpected operator", input[i]);
+					free(buf.str);
+					return NULL;
+				}
 
 				continue;
 			}
